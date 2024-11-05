7e4d64ed9e379b1f194ae6abd109eafb94a9d9b8
Maria Julia Germano <166447580+MariaJuliaGermano@users.noreply.github.com>
2024-11-05 11:10:10 -0300

    Add struct JogoForca for game state management

--- include/game.h
+++ include/game.h
@@ -1,6 +1,17 @@
 #ifndef GAME_H
 #define GAME_H
 
-void iniciar_jogo();
+typedef struct {
+    char palavra_secreta[20];
+    int tentativas_restantes;
+    char letras_acertadas[20];
+    int pontuacao;
+} JogoForca;
+
+void start_game();
+void inicializa_palavra(JogoForca *jogo);
+void exibir_status(JogoForca *jogo);
+int verifica_letra(char letra, JogoForca *jogo);
+int jogo_completo(JogoForca *jogo);
 
 #endif
