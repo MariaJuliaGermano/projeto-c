85ccdb2ab9cba8769971ef33ab7aea53cdcf2a3b
Maria Julia Germano <166447580+MariaJuliaGermano@users.noreply.github.com>
2024-11-18 11:42:29 -0300

    Update keyboard

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,6 +1,22 @@
 #include "keyboard.h"
-#include <stdio.h> // CLI-lib
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
 
 char get_key() {
-    return getchar(); // Captura teclas do terminal (simulação realista).
+    struct termios oldt, newt;
+    char ch;
+
+    // Desativa o buffer de entrada
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    // Captura o caractere pressionado
+    ch = getchar();
+
+    // Restaura as configurações do terminal
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
 }
